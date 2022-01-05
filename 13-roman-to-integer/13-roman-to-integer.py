bce0b304b0f1f5f315b8827abc34ed06cefc1a5c
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-01-06 01:06:50 +0530

    Time: 137 ms (5.10%), Space: 14.5 MB (28.41%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,36 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        rec = {
+            'CD': 400,
+            'CM': 900,
+            'XL': 40,
+            'XC': 90,
+            'IV': 4,
+            'IX': 9,
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1
+        }
+        
+        out = 0
+        while len(s) != 0:
+            if s[0:3] in rec.keys():
+                out += int(rec[s[0:3]])
+                print(s[0:3], int(rec[s[0:3]]))
+                s = s[3::]
+                continue
+            if s[0:2] in rec.keys():
+                out += int(rec[s[0:2]])
+                print(s[0:2], int(rec[s[0:2]]))
+                s = s[2::]
+                continue
+            if s[0:1] in rec.keys():
+                out += int(rec[s[0:1]])
+                print(s[0:1], int(rec[s[0:1]]))
+                s = s[1::]
+                continue
+        return out
