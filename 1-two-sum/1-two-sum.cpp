a662f83643126ad7e2cf19c3f655d1b28a970020
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-01-26 00:55:00 +0530

    Time: 18 ms (58.25%), Space: 12 MB (11.61%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         // creating complement dictionary:
-        std::map<int, int> rec;
+        std::unordered_map<int, int> rec;
         
         // filling the complement dictionary:
         for (int ix = 0; ix < nums.size(); ix++)
-            rec.insert(std::pair<int, int>(target - nums[ix], ix));
+            rec[target - nums[ix]] = ix;
         
         // search if complement exist and index is different:
         for (int ix = 0; ix < nums.size(); ix++) {
-            if (rec.find(nums[ix]) != rec.end()) {
+            if (rec.count(nums[ix]) > 0) {
                 if (ix != rec[nums[ix]]) {
                     return std::vector<int>({ix, rec[nums[ix]]});
                 }
