89a1e2a56be7ee998dd6bb043fb97b24fe7763ff
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-01-26 00:57:37 +0530

    Time: 22 ms (51.67%), Space: 10.7 MB (56.11%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -6,16 +6,11 @@ public:
         std::unordered_map<int, int> rec;
         
         // filling the complement dictionary:
-        for (int ix = 0; ix < nums.size(); ix++)
-            rec[target - nums[ix]] = ix;
-        
-        // search if complement exist and index is different:
         for (int ix = 0; ix < nums.size(); ix++) {
             if (rec.count(nums[ix]) > 0) {
-                if (ix != rec[nums[ix]]) {
-                    return std::vector<int>({ix, rec[nums[ix]]});
-                }
+                return std::vector<int>({ix, rec[nums[ix]]});
             }
+            rec[target - nums[ix]] = ix;
         }
         
         // return empty vector if solution does not exist:
