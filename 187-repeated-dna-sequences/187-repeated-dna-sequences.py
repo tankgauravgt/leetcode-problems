18a257ee4aa1a8bdea54ad28ed8f8608286d00a0
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-03-10 12:06:52 +0530

    Time: 110 ms (41.24%), Space: 27.8 MB (11.96%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -1,16 +1,19 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         
-        if len(s) < 10:
+        if len(s) < 11:
             return []
         
         rec = {}
-        for n in range(len(s) - 9):
-            if s[n:n+10] not in rec.keys():
-                rec.update({s[n:n+10]: 1})
+        for ix in range(len(s) - 10 + 1):
+            if s[ix:ix + 10] not in rec:
+                rec[s[ix:ix+10]] = 1
             else:
-                rec[s[n:n+10]] += 1
+                rec[s[ix:ix+10]] = rec[s[ix:ix+10]] + 1
         
-        print(rec)
+        out = []
+        for candidate in filter(lambda x: x[1] > 1, rec.items()):
+            if candidate:
+                out += [candidate[0]]
         
-        return list(dict(filter(lambda x: x[1] > 1, rec.items())).keys())
+        return out
