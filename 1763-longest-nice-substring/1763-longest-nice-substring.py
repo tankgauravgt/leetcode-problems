2611d4c138b08a58aa9f12d2a83153298827fa3b
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-03-11 17:47:12 +0530

    Time: 202 ms (28.45%), Space: 13.9 MB (85.09%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -1,21 +1,23 @@
 class Solution:
     
     def is_nice(self, s):
-        
-        r = {c: 1 for c in set(s)}        
-        for c in set(s):
-            if r.get(c.lower(), 0) + r.get(c.upper(), 0) != 2:
-                return False
-        return True
+        if len(set(s)) == (2 * len(set(s.lower()))):
+            return True
+        return False
     
     
     def longestNiceSubstring(self, s: str) -> str:
         
-        l = 0
-        r = -1
-        for lx in range(len(s) - 1):
-            for rx in range(len(s)):
-                if self.is_nice(s[lx:rx+1]):
+        if len(s) < 2:
+            return ""
+        
+        l, r = 0, 0
+        for lx in range(0, len(s), 1):
+            for rx in range(lx, 1 + len(s), 1):
+                if self.is_nice(s[lx:rx]):
                     if (r - l + 1) < (rx - lx + 1):
-                        l, r = lx, rx        
-        return s[l:r+1]
+                        l, r = lx, rx
+                else:
+                    pass
+        
+        return s[l:r]
