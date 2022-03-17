54ea72339a28a3fb6990604be57029b690e053bd
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-03-18 05:12:24 +0530

    Time: 503 ms (17.36%), Space: 19.5 MB (5.05%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        result = []
+        current = []
+        self.backtrack(nums, current, result)
+        return list(set(map(lambda e: tuple(e), result)))
+    
+    
+    def backtrack(self, remaining, current, result):
+        if len(remaining) == 0:
+            result += [current]
+            return
+        
+        for ix in range(len(remaining)):
+            remaining[0], remaining[ix] = remaining[ix], remaining[0]
+            current_copy = current + [remaining[0]]
+            self.backtrack(remaining[1::], current_copy, result)
+            remaining[0], remaining[ix] = remaining[ix], remaining[0]
