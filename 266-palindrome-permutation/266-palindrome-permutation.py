6ea37d12b42ad6c754b1b34d8c861a50bbe48229
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-03-23 04:40:56 +0530

    Time: 32 ms (86.05%), Space: 13.8 MB (97.24%) - LeetHub

--- 266-palindrome-permutation/266-palindrome-permutation.py
+++ 266-palindrome-permutation/266-palindrome-permutation.py
@@ -2,9 +2,6 @@ from collections import Counter
 
 class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
-        
-        # print(list(filter(lambda x: x % 2, dict(Counter(s)).values())))
-        
         if len(s) % 2 == 0:
             return len(list(filter(lambda x: x % 2, dict(Counter(s)).values()))) == 0
         else:
