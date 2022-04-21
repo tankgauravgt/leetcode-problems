fb721e18c1df8ed88600251079baa7d50ec48113
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-04-21 09:03:26 +0530

    Time: 47 ms (48.67%), Space: 13.9 MB (57.57%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -5,9 +5,7 @@ class Solution:
         counter = 0
         for ix, n in enumerate(nums):
             if n in rec:
-                for i in rec[n]:
-                    if ix > i:
-                        counter += 1
+                counter += len(rec[n])
                 rec[n] += [ix]
             else:
                 rec[n] = [ix]
