78cb30dde59fd1e8e7fb41f1ae826f7c74bdb29f
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-05-31 10:52:37 +0530

    Time: 67 ms (29.77%), Space: 14.2 MB (91.38%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,21 +1,23 @@
 class Solution:
-    
-    def binSearchRotated(self, arr, target, sx, ex):
-
+        
+    def search(self, nums: List[int], target: int) -> int:
+        
+        sx = 0
+        ex = len(nums) - 1
+        
         while sx <= ex:
             
             mx = sx + ((ex - sx) // 2)
-            if arr[mx] == target:
-                return mx
+            if nums[mx] == target:
+                return mx       
             
-            
-            if arr[sx] < arr[mx]:
-                if target < arr[sx] or target > arr[mx]:
+            if nums[sx] < nums[mx]:
+                if target < nums[sx] or target > nums[mx]:
                     sx = mx + 1
                 else:
                     ex = mx - 1
-            elif arr[sx] > arr[mx]:
-                if target > arr[ex] or target < arr[mx]:
+            elif nums[sx] > nums[mx]:
+                if target > nums[ex] or target < nums[mx]:
                     ex = mx - 1
                 else:
                     sx = mx + 1
@@ -23,10 +25,3 @@ class Solution:
                 sx = sx + 1
             
         return -1
-        
-    def search(self, nums: List[int], target: int) -> int:
-        return self.binSearchRotated(nums, target, 0, len(nums) - 1)
-        
-#         0 1 2 3 4 5 6
-#         4 5 6 7 0 1 2 | 0
-#         s     m     e
