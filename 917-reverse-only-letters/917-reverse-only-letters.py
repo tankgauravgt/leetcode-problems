dbe1de67b4d3276638fee3756ccf1adf3fdc13e4
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-06-06 03:39:05 +0530

    Time: 32 ms (85.07%), Space: 13.9 MB (62.73%) - LeetHub

--- 917-reverse-only-letters/917-reverse-only-letters.py
+++ 917-reverse-only-letters/917-reverse-only-letters.py
@@ -5,17 +5,14 @@ class Solution:
         
         lx = 0
         rx = len(s) - 1
-        
-        rec_a = set(range(ord('A'), 1 + ord('Z')))
-        rec_b = set(range(ord('a'), 1 + ord('z')))
-        rec = rec_a | rec_b
+        rec = set('abcdefghijklmnopqrstuvwxyz') | set('abcdefghijklmnopqrstuvwxyz'.upper())
         
         while lx < rx:
-            if ord(s[lx]) not in rec:
+            if s[lx] not in rec:
                 lx = lx + 1
-            if ord(s[rx]) not in rec:
+            if s[rx] not in rec:
                 rx = rx - 1
-            if ord(s[lx]) in rec and ord(s[rx]) in rec:
+            if s[lx] in rec and s[rx] in rec:
                 s[lx], s[rx] = s[rx], s[lx]
                 lx = lx + 1
                 rx = rx - 1
