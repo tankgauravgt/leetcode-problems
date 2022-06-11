8fbc30016679108209330a81eff9065d293c8372
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-06-11 21:18:03 +0530

    Time: 233 ms (12.52%), Space: 14.5 MB (12.96%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,30 +1,4 @@
-class Solution:
-    
-    def clash(self, row, col, N):
-        
-        vc = 0
-        for rx in range(row):
-            if self.mat[rx][col] == 'Q':
-                return True
-        
-        ix = row
-        jx = col
-        while ix >= 0 and jx >= 0:
-            if self.mat[ix][jx] == 'Q':
-                return True
-            ix = ix - 1
-            jx = jx - 1
-        
-        ix = row
-        jx = col
-        while ix >= 0 and jx < N:
-            if self.mat[ix][jx] == 'Q':
-                return True
-            ix = ix - 1
-            jx = jx + 1
-        
-        return False
-        
+class Solution:        
     
     def backtrack(self, row, N):
         if row == N:
@@ -38,13 +12,56 @@ class Solution:
     
     def solveNQueens(self, n: int) -> List[List[str]]:
         
-        # create board:
-        self.mat = [['.'] * n for ix in range(n)]
+        out = []
+        mat = [['.'] * n for ix in range(n)]        
         
-        # create result:
-        self.result = []
+        
+        # =======================================
+        # check for clash if any:
+        # =======================================
+        
+        def clash(row, col, N):
+            # check for column clash:
+            vc = 0
+            for rx in range(row):
+                if mat[rx][col] == 'Q':
+                    return True
+
+            # check for left diagonal clash:
+            ix = row
+            jx = col
+            while ix >= 0 and jx >= 0:
+                if mat[ix][jx] == 'Q':
+                    return True
+                ix = ix - 1
+                jx = jx - 1
+
+            # check for right diagonal clash:
+            ix = row
+            jx = col
+            while ix >= 0 and jx < N:
+                if mat[ix][jx] == 'Q':
+                    return True
+                ix = ix - 1
+                jx = jx + 1
+            
+            return False
+
+        
+        # ========================================
+        # backtrack and find solution:
+        # ========================================
+        
+        def backtrack(row, N):
+            if row == N:
+                out.append(["".join(rarr) for rarr in mat])
+            for cx in range(N):
+                if not clash(row, cx, N):
+                    mat[row][cx] = 'Q'
+                    backtrack(row + 1, N)
+                    mat[row][cx] = '.'
         
         # call solveNQueens:
-        self.backtrack(0, n)
+        backtrack(0, n)
         
-        return self.result
+        return out
