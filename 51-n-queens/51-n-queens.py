4d5dc3ee29a34b3af12906dbb5f27429b5504323
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-06-11 21:01:04 +0530

    Time: 260 ms (11.05%), Space: 14.4 MB (82.08%) - LeetHub

---
+++ 51-n-queens/51-n-queens.py
@@ -0,0 +1,50 @@
+class Solution:
+    
+    def clash(self, row, col, N):
+        
+        vc = 0
+        for rx in range(row):
+            if self.mat[rx][col] == 'Q':
+                return True
+        
+        ix = row
+        jx = col
+        while ix >= 0 and jx >= 0:
+            if self.mat[ix][jx] == 'Q':
+                return True
+            ix = ix - 1
+            jx = jx - 1
+        
+        ix = row
+        jx = col
+        while ix >= 0 and jx < N:
+            if self.mat[ix][jx] == 'Q':
+                return True
+            ix = ix - 1
+            jx = jx + 1
+        
+        return False
+        
+    
+    def backtrack(self, row, N):
+        if row == N:
+            self.result += [["".join(rarr) for rarr in self.mat.copy()]]
+        for cx in range(N):
+            if not self.clash(row, cx, N):
+                self.mat[row][cx] = 'Q'
+                self.backtrack(row + 1, N)
+                self.mat[row][cx] = '.'
+    
+    
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        
+        # create board:
+        self.mat = [['.'] * n for ix in range(n)]
+        
+        # create result:
+        self.result = []
+        
+        # call solveNQueens:
+        self.backtrack(0, n)
+        
+        return self.result
