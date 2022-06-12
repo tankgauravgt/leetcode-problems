d76515025803f02804142c48b0a87a10c0120faf
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-06-13 02:07:56 +0530

    Time: 155 ms (32.66%), Space: 14 MB (30.33%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -2,18 +2,19 @@ class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         
         res = 0
-        def recurse(arr, buf, k):
+        buf = []
+        def recurse(arr, k):
+            nonlocal buf, res
             if k == len(arr):
                 tmp = 0
-                nonlocal res
                 for n in buf:
                     tmp = (tmp ^ n)
                 res = res + tmp
                 return
             buf.append(nums[k])
-            recurse(arr, buf, k + 1)
+            recurse(arr, k + 1)
             buf.pop()
-            recurse(arr, buf, k + 1)
+            recurse(arr, k + 1)
         
-        recurse(nums, [], 0)
+        recurse(nums, 0)
         return res
