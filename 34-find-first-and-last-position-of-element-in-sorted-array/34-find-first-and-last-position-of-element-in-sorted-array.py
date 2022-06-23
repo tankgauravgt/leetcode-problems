9fb7a46d12e00ae2b76d5e72f826c6c05f27470b
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-06-23 18:37:15 +0530

    Time: 140 ms (33.50%), Space: 15.4 MB (92.84%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,11 @@
+import bisect
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
+        lx = bisect.bisect_left(nums, target, 0, len(nums))
+        rx = bisect.bisect_right(nums, target, 0, len(nums))
+        if lx < len(nums) and nums[lx] == target:
+            return [lx, rx - 1]
+        else:
+            return [-1, -1]
