c316c8f580a4b5542cee2afde2f7242316ec4d5b
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-07-04 13:58:43 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (47.64%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    
-    int dnc(uint32_t k) {
-        return (k == 0 ? 0: 1 + dnc(k & (k - 1)));
-    }
-    
     int hammingWeight(uint32_t n) {
-        return dnc(n);
+        uint32_t count = 0;
+        while(n) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
     }
 };
