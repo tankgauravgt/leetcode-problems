98457e0b9d1e3db4effff8a90c048064a43e18fc
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-07-04 23:14:45 +0530

    Time: 1158 ms (5.00%), Space: 18.4 MB (5.31%) - LeetHub

--- 372-super-pow/372-super-pow.py
+++ 372-super-pow/372-super-pow.py
@@ -9,10 +9,10 @@ class Solution:
             elif b % 2 == 0:
                 if b // 2 not in memo:
                     memo[b // 2] = dNc(a, b // 2, memo) % 1337
-                return (memo[b // 2] % 1337) * (memo[b // 2] % 1337)
+                return (memo[b // 2] % 1337) * (memo[b // 2] % 1337) % 1337
             else:
                 if b // 2 not in memo:
                     memo[b // 2] = (dNc(a, b // 2, memo) % 1337)
-                return a * (memo[b // 2] % 1337) * (memo[b // 2] % 1337)
+                return (a * (memo[b // 2] % 1337) * (memo[b // 2] % 1337)) % 1337
         
         return dNc(a, b, {}) % 1337
