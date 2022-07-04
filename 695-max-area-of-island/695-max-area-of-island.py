9f0ea956f93b89900b82a2aabc810156c8bafdd2
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-07-05 00:38:31 +0530

    Time: 182 ms (67.80%), Space: 16.6 MB (47.25%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        
+        def bfs(rx, cx):
+            nonlocal grid, count
+            grid[rx][cx] = 0
+            count = count + 1
+            if rx < len(grid) - 1 and grid[rx + 1][cx] == 1:
+                bfs(rx + 1, cx)
+            if cx < len(grid[0]) - 1 and grid[rx][cx + 1] == 1:
+                bfs(rx, cx + 1)
+            if rx > 0 and grid[rx - 1][cx] == 1:
+                bfs(rx - 1, cx)
+            if cx > 0 and grid[rx][cx - 1] == 1:
+                bfs(rx, cx - 1)
+        
+        count = 0
+        max_area = 0
+        for ix in range(len(grid)):
+            for jx in range(len(grid[0])):
+                if grid[ix][jx] == 1:
+                    count = 0
+                    bfs(ix, jx)
+                    max_area = max(count, max_area)
+        
+        return max_area
