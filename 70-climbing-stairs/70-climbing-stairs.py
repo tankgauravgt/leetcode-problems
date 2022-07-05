d2ee4d231b18d1dd232b254b86eaee2d4a34bf8f
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-07-06 00:12:53 +0530

    Time: 61 ms (11.94%), Space: 13.8 MB (57.08%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,14 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        def dp(n, memo):
-            if n <= 2:
-                return n
-            else:
-                if (n - 1) not in memo:
-                    memo[n - 1] = dp(n - 1, memo)
-                if (n - 2) not in memo:
-                    memo[n - 2] = dp(n - 2, memo)
-                return memo[n - 1] + memo[n - 2]
+        if n == 1:
+            return 1
         
-        return dp(n, {})
+        t1 = 1
+        t2 = 2
+        for ix in range(n - 2):
+            t1, t2 = t2, t1 + t2
+        return t2
