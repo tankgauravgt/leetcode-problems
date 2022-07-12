41d96bcda3211fbbc55bfacd376563b70842ae20
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-07-12 19:27:43 +0530

    Time: 1130 ms (16.62%), Space: 20.1 MB (8.58%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,29 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        
+        def feasible(cap):
+            nonlocal weights
+            lx = 0
+            for jx in range(days):
+                curr = 0
+                while lx < len(weights) and curr <= cap:
+                    if curr + weights[lx] <= cap:
+                        curr += weights[lx]
+                        lx = lx + 1
+                    else:
+                        break
+            return (lx >= len(weights))
+        
+        def binsearch(lx, rx):
+            if rx >= lx:
+                mx = lx + (rx - lx) // 2
+                if feasible(mx):
+                    tx = binsearch(lx, mx - 1)
+                    if tx != -1:
+                        return tx
+                    return mx
+                else:
+                    return binsearch(mx + 1, rx)
+            return -1
+        
+        return binsearch(weights[-1], sum(weights))
