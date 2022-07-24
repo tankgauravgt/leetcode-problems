a2353a642ff323b4de666c69a3e6cd7667dd8d28
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-07-25 02:11:05 +0530

    Time: 72 ms (11.81%), Space: 14 MB (60.43%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -8,15 +8,15 @@ class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         
         def rec(r1, r2):
-            if r1 and r2:
+            if not r1 and not r2:
+                return True
+            elif (r1 and not r2) or (r2 and not r1):
+                return False
+            else:
                 if r1.val != r2.val:
                     return False
                 lval = rec(r1.left, r2.right)
                 rval = rec(r1.right, r2.left)
                 return True and lval and rval
-            if (r1 and not r2) or (r2 and not r1):
-                return False
-            if not r1 and not r2:
-                return True
         
         return rec(root, root)
