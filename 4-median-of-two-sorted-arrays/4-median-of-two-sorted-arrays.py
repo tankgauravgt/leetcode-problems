5023d864c8a68576f8f96a76dd34450f8f1d636d
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-07-25 01:01:20 +0530

    Time: 102 ms (87.87%), Space: 15 MB (5.83%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -11,14 +11,14 @@ class Solution:
         hL = fL // 2
         
         def bin_median(lx, rx):
-            nonlocal nums1, nums2, hL, fL
             
-            mx1 = (rx + lx) // 2
+            mx1 = (lx + rx) // 2
             mx2 = hL - mx1 - 2
             
             a1 = -float('inf') if mx1 < 0 else nums1[mx1]
-            a2 = float('inf') if (1 + mx1) >= len(nums1) else nums1[1 + mx1]
             b1 = -float('inf') if mx2 < 0 else nums2[mx2]
+            
+            a2 = float('inf') if (1 + mx1) >= len(nums1) else nums1[1 + mx1]
             b2 = float('inf') if (1 + mx2) >= len(nums2) else nums2[1 + mx2]
             
             if a1 <= b2 and b1 <= a2:
@@ -31,5 +31,5 @@ class Solution:
                     return bin_median(lx, mx1 - 1)
                 else:
                     return bin_median(mx1 + 1, rx)
-        
+            
         return bin_median(0, len(nums1) - 1)
