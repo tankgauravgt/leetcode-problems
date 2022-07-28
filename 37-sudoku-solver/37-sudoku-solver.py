b40c863f3bb42caccc3403ecf01bfb06a0db0f3a
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-07-28 20:06:10 +0530

    Time: 1527 ms (5.28%), Space: 14.1 MB (43.77%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.py
@@ -0,0 +1,38 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        nR = len(board)
+        nC = len(board[0])
+        
+        def isValid(rid, cid, num):
+            nonlocal board, nR, nC
+            for ix in range(nR):
+                if board[ix][cid] == num:
+                    return False
+                if board[rid][ix] == num:
+                    return False
+                bir, bic = (rid // 3), (cid // 3)
+                if board[(3 * bir) + (ix // 3)][(3 * bic) + (ix % 3)] == num:
+                    return False
+            return True
+        
+        
+        def backtrack():
+            nonlocal board, nR, nC
+            for rx in range(nR):
+                for cx in range(nC):
+                    if board[rx][cx] == '.':
+                        for n in range(1, 10):
+                            if isValid(rx, cx, str(n)):
+                                board[rx][cx] = str(n)
+                                if backtrack():
+                                    return True
+                                else:
+                                    board[rx][cx] = '.'
+                        return False
+            return True
+        
+        backtrack()
