10b6318131a27f162d3dd8664106e86ab90ee189
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-07-29 16:27:16 +0530

    Time: 67 ms (21.99%), Space: 13.8 MB (74.54%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -7,6 +7,6 @@ class Solution:
         m = matrix        
         N = len(matrix)
         
-        for rx in range(N // 2):
-            for cx in range((1 + N) // 2):
+        for rx in range((1 + N) // 2):
+            for cx in range(N // 2):
                 m[rx][cx], m[N-cx-1][rx], m[N-rx-1][N-cx-1], m[cx][N-rx-1] = m[N-cx-1][rx], m[N-rx-1][N-cx-1], m[cx][N-rx-1], m[rx][cx]
