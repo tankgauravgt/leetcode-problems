45b99a4ae9aa9181423d0d893a9db525c1750493
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-08-07 02:01:47 +0530

    Time: 92 ms (83.93%), Space: 17.6 MB (15.93%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -7,16 +7,23 @@ class Solution:
         insert_ix = bisect.bisect_left(intervals, newInterval[0], 0, len(intervals), key=lambda x: x[0])
         intervals = intervals[0:insert_ix] + [newInterval] + intervals[insert_ix::]
         
-        res = []
-        last = intervals[0]
-        for curr in intervals[1::]:
-            if curr[0] < last[1] and curr[1] <= last[1]:
-                continue
-            elif curr[0] <= last[1] and curr[1] >= last[1]:
-                last[1] = curr[1]
-            else:
-                res.append(last)
-                last = curr
-        res.append(last)
+        # [1, 3], [2, 5], [6, 9]
+        # ======================================
+        # ix = 0, lx = 0: [1, 3]
+        # ix = 1, lx = 0: [1, 5], [2, 5],
+        # ix = 2, lx = 0: [1, 5], [2, 5], [6, 9]
         
-        return res
+        
+        lx = 0
+        for ix, curr in enumerate(intervals):
+            if ix == 0:
+                continue
+            if intervals[lx][1] >= curr[0] and intervals[lx][1] < curr[1]:
+                intervals[lx][1] = curr[1]
+            elif intervals[lx][1] < curr[0]:
+                lx = lx + 1
+                intervals[lx] = curr
+            else:
+                continue
+        
+        return intervals[0:1 + lx]
