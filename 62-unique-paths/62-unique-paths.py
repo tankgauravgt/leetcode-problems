c4d028cfbfd28546d4293fa079cb0c5ba4ee74c9
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-08-07 17:18:37 +0530

    Time: 38 ms (81.67%), Space: 13.7 MB (98.12%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -2,10 +2,5 @@ import math
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
-        if m < n:
-            return self.uniquePaths(n, m)
-        
-        f = math.factorial
-        
+        f = math.factorial        
         return int(f(m + n - 2) / (f(m - 1) * f(n - 1)))
