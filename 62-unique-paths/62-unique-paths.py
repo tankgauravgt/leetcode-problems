9fce40b9bba7af9f9da5acb6118178ecb901fffb
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-08-07 17:22:14 +0530

    Time: 49 ms (52.07%), Space: 13.9 MB (73.95%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -2,5 +2,16 @@ import math
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        f = math.factorial        
-        return int(f(m + n - 2) / (f(m - 1) * f(n - 1)))
+        
+        dp = [[0] * n for mx in range(m)]
+        
+        for mx in range(m):
+            dp[mx][0] = 1
+        for nx in range(n):
+            dp[0][nx] = 1
+        
+        for mx in range(1, m):
+            for nx in range(1, n):
+                dp[mx][nx] = dp[mx - 1][nx] + dp[mx][nx - 1]
+        
+        return dp[m - 1][n - 1]
