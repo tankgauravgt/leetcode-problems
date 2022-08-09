db78b0ae1f9867254eee9bab7d50ce8bb6469381
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-08-09 17:25:35 +0530

    Time: 35 ms (88.48%), Space: 14 MB (8.10%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,5 +1,7 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         def dnc(k):
-            return 0 if k == 0 else 1 + dnc(k & (k - 1))
+            if k == 0:
+                return 0
+            return (k % 2) + dnc(k // 2)
         return dnc(n)
