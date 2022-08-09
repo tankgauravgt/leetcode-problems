269337dd49fdafc86fa05015c40cc3a2c5edad8b
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-08-10 02:30:54 +0530

    Time: 41 ms (76.59%), Space: 14.5 MB (6.40%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -3,21 +3,22 @@ class Solution:
         
         rec = {str(1 + ix):chr(ix + ord('a')) for ix in range(26)}
         
-        memo = [-1] * (1 + len(s))
+        memo = {}
         def dfs(s, sx):
             nonlocal memo
             if sx == len(s):
                 return 1
-            elif memo[sx] != -1:
+            elif sx in memo:
                 return memo[sx]
             else:
                 total = 0
                 ex = 1 + sx
                 while ex <= len(s) and s[sx:ex] in rec:
-                    if memo[ex] == -1:
+                    if ex not in memo:
                         memo[ex] = dfs(s, ex)
                     total += memo[ex]
                     ex = ex + 1
+                memo[sx] = total
                 return total
         
         return dfs(s, 0)
