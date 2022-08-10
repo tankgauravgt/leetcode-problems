4d254edc3001d18900e73dfe753463587a9c23c8
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-08-10 14:16:30 +0530

    Time: 8489 ms (32.48%), Space: 13.9 MB (92.81%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,33 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        found = False
+        def btrack(ix, jx, px, word):
+            nonlocal found
+            if px == len(word):
+                found = True
+                return
+            if ix not in range(len(board)):
+                return
+            if jx not in range(len(board[0])):
+                return
+            if board[ix][jx] == word[px]:
+                board[ix][jx] = '.'
+                btrack(ix + 1, jx, 1 + px, word)
+                if found: return
+                btrack(ix - 1, jx, 1 + px, word)
+                if found: return
+                btrack(ix, jx + 1, 1 + px, word)
+                if found: return
+                btrack(ix, jx - 1, 1 + px, word)
+                if found: return
+                board[ix][jx] = word[px]
+            else:
+                return
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0]:
+                    btrack(i, j, 0, word)
+        
+        return found
