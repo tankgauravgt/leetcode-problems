9cae5e6d870db5b370455df3289644bbdfc72472
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-08-14 00:14:47 +0530

    Time: 89 ms (72.56%), Space: 13.9 MB (93.22%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -3,10 +3,10 @@ from collections import Counter
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         
+        res = []
         rec = dict(Counter(candidates))
         rec = [(n, freq) for n, freq in rec.items()]
         
-        res = []
         def btrack(cx, rem, buf):
             nonlocal res
             if rem <= 0:
@@ -14,13 +14,14 @@ class Solution:
                     res.append(buf.copy())
                 return
             for ix in range(cx, len(rec)):
-                n, freq = rec[ix]
-                if n <= rem and freq > 0:
-                    buf.append(n)
-                    rec[ix] = (n, freq - 1)
-                    btrack(ix, rem - n, buf)
-                    rec[ix] = (n, freq)
-                    buf.pop()
+                if rec[ix][0] <= rem:
+                    val, freq = rec[ix]
+                    if freq > 0:
+                        buf.append(val)
+                        rec[ix] = (val, freq - 1)
+                        btrack(ix, rem - val, buf)
+                        rec[ix] = (val, freq)
+                        buf.pop()
         
         btrack(0, target, [])
         return res
