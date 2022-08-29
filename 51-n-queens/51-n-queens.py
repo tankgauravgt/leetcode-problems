f5baf3fe9d47691a2f690ed5a072edd2d75ab6b3
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-08-29 17:59:40 +0530

    Time: 153 ms (31.54%), Space: 14.5 MB (13.82%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -2,53 +2,40 @@ class Solution:
     
     def solveNQueens(self, n: int) -> List[List[str]]:
         
-        out = []
-        mat = [['.'] * n for ix in range(n)]        
-                
-        # =======================================
-        # check for clash if any:
-        # =======================================
-        
-        def clash(row, col, N):
-            # check for column clash:
-            vc = 0
-            for rx in range(row):
-                if mat[rx][col] == 'Q':
-                    return True
-
-            # check for left diagonal clash:
-            ix = row
-            jx = col
-            while ix >= 0 and jx >= 0:
-                if mat[ix][jx] == 'Q':
-                    return True
-                ix = ix - 1
-                jx = jx - 1
-
-            # check for right diagonal clash:
-            ix = row
-            jx = col
-            while ix >= 0 and jx < N:
-                if mat[ix][jx] == 'Q':
-                    return True
-                ix = ix - 1
-                jx = jx + 1
+        def valid(rx, cx):
+            for r in range(0, rx):
+                if grid[r][cx] == "Q":
+                    return False
+            r, c = rx, cx
+            while r >= 0 and c >= 0:
+                if grid[r][c] == "Q":
+                    return False
+                r = r - 1
+                c = c - 1
             
-            return False
-
+            r, c = rx, cx
+            while r < nR and c < nC:
+                if grid[r][c] == "Q":
+                    return False
+                r = r - 1
+                c = c + 1
+            return True
         
-        # ========================================
-        # backtrack and find solution:
-        # ========================================
         
-        def backtrack(row, N):
-            if row == N:
-                out.append(["".join(rarr) for rarr in mat])
-            for cx in range(N):
-                if not clash(row, cx, N):
-                    mat[row][cx] = 'Q'
-                    backtrack(row + 1, N)
-                    mat[row][cx] = '.'
+        nR = nC = n
+        grid = [["."] * nC for _ in range(nR)]
         
-        backtrack(0, n)
-        return out
+        res = []
+        def solve(rx):
+            nonlocal grid
+            if rx == nR:
+                res.append(["".join(row) for row in grid])
+                return
+            for c in range(nC):
+                if valid(rx, c):
+                    grid[rx][c] = "Q"
+                    solve(1 + rx)
+                    grid[rx][c] = "."
+        
+        solve(0)
+        return res
