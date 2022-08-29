4a22eec24c3d871a6fed62c267f63cf47d0a4c5f
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-08-29 23:16:37 +0530

    Time: 63 ms (9.50%), Space: 13.9 MB (57.09%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,11 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        if n < 2:
-            return n
+        if n < 2: return n
         
-        l1 = 1
-        l2 = 2
+        l1, l2 = 1, 2
         for ix in range(n - 2):
             l2, l1 = l1 + l2, l2
         return l2
