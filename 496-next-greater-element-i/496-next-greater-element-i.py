f55325d66c0f4ad2054a5f22a7558cd9beade2f2
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-08-31 23:13:32 +0530

    Time: 46 ms (97.78%), Space: 14.3 MB (15.37%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        res = [-1] * len(nums1)
+        
+        rec = {n:ix for ix, n in enumerate(nums1)}
+        
+        stk = []
+        for ix, n in enumerate(nums2):
+            while stk and stk[-1] < n:
+                val = stk.pop()
+                if val in rec:
+                    res[rec[val]] = n
+            stk.append(n)
+        
+        return res
