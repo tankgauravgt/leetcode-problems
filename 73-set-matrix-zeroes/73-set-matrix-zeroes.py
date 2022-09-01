ff631b666e73a73b41130399595a62838838fe74
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-09-01 18:42:56 +0530

    Time: 209 ms (47.89%), Space: 14.7 MB (54.54%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -7,39 +7,42 @@ class Solution:
         cstat = 1
         rstat = 1
         
+        nR = len(matrix)
+        nC = len(matrix[0])
+        
         # column status:
-        for cx in range(len(matrix[0])):
+        for cx in range(nC):
             if matrix[0][cx] == 0:
                 cstat = 0
         
         # row status:
-        for rx in range(len(matrix)):
+        for rx in range(nR):
             if matrix[rx][0] == 0:
                 rstat = 0
         
         # submatrix status:
-        for rx in range(1, len(matrix)):
-            for cx in range(1, len(matrix[0])):
+        for rx in range(1, nR):
+            for cx in range(1, nC):
                 if matrix[rx][cx] == 0:
                     matrix[rx][0] = 0
                     matrix[0][cx] = 0
                 
         # clear rows:
-        for rx in range(1, len(matrix)):
+        for rx in range(1, nR):
             if matrix[rx][0] == 0:
-                for cx in range(1, len(matrix[0])):
+                for cx in range(1, nC):
                     matrix[rx][cx] = 0
         
         # clear columns:
-        for cx in range(1, len(matrix[0])):
+        for cx in range(1, nC):
             if matrix[0][cx] == 0:
                 for rx in range(1, len(matrix)):
                     matrix[rx][cx] = 0
         
         if cstat == 0:
-            for cx in range(0, len(matrix[0])):
+            for cx in range(0, nC):
                 matrix[0][cx] = 0
         
         if rstat == 0:
-            for rx in range(0, len(matrix)):
+            for rx in range(0, nR):
                 matrix[rx][0] = 0
