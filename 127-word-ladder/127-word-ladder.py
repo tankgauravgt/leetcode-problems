3212fba207b8f07a89e5aff43f88dd83b761781d
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-09-09 22:58:53 +0530

    Time: 1387 ms (8.24%), Space: 15.1 MB (71.14%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
+        wordset = set(wordList)
+        
+        rec = set([beginWord])
+        def get_neighbors(word):
+            neighbors = []
+            for ix in range(len(word)):
+                for c in range(ord('a'), 1 + ord('z')):
+                    temp = list(word)
+                    temp[ix] = chr(c)
+                    candidate = "".join(temp)
+                    if candidate in wordset and candidate not in rec:
+                        neighbors.append("".join(temp))
+            return neighbors
+        
+        count = 1
+        dq = deque([beginWord])
+        while dq:
+            N = len(dq)
+            for ix in range(N):
+                temp = dq.popleft()
+                if temp == endWord:
+                    return count
+                for adj in get_neighbors(temp):
+                    if adj not in rec:
+                        rec.add(adj)
+                        dq.append(adj)
+            count = count + 1
+        
+        return 0
