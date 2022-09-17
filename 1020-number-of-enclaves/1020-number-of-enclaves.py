54fa7bfd08704a43da5001200e48e210fc8ce49c
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-09-17 15:07:16 +0530

    Time: 2739 ms (5.01%), Space: 21.8 MB (22.76%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,28 +1,46 @@
+from collections import deque
+
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         
-        def dfs(rx, cx):
-            nonlocal grid, valid, count
-            if (rx == 0) or (cx == 0) or (rx == len(grid) - 1) or (cx == len(grid[0]) - 1):
-                valid = False
-            count = count + 1
-            grid[rx][cx] = 0
-            if rx > 0 and grid[rx - 1][cx] == 1:
-                dfs(rx - 1, cx)
-            if rx < len(grid) - 1 and grid[rx + 1][cx] == 1:
-                dfs(rx + 1, cx)
-            if cx > 0 and grid[rx][cx - 1] == 1:
-                dfs(rx, cx - 1)
-            if cx < len(grid[0]) - 1 and grid[rx][cx + 1] == 1:
-                dfs(rx, cx + 1)
+        nR = len(grid)
+        nC = len(grid[0])
         
-        total_count = 0
-        for ix in range(len(grid)):
-            for jx in range(len(grid[0])):
+        def adjs(r, c):
+            drdc = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+            buf = []
+            for dr, dc in drdc:
+                if (r + dr) in range(0, nR) and (c + dc) in range(0, nC):
+                    if grid[r + dr][c + dc] == 1:
+                        buf.append((r + dr, c + dc))
+            return buf
+        
+        
+        vrec = set()
+        def bfs(sr, sc):
+            
+            if (sr, sc) in vrec:
+                return (False, 0)
+            
+            ctr = 0
+            valid = True
+            vrec.add((sr, sc))
+            dq = deque([(sr, sc)])
+            while dq:
+                rr, cc = dq.popleft()
+                ctr = ctr + 1
+                if rr == 0 or rr == (nR - 1) or cc == 0 or cc == (nC - 1):
+                    valid = False
+                for adj in adjs(rr, cc):
+                    if adj not in vrec:
+                        vrec.add(adj)
+                        dq.append(adj)
+            return (valid, ctr)
+        
+        total = 0
+        for ix in range(1, nR):
+            for jx in range(1, nC):
                 if grid[ix][jx] == 1:
-                    count = 0
-                    valid = True
-                    dfs(ix, jx)
-                    if valid:
-                        total_count += count
-        return total_count
+                    res = bfs(ix, jx)
+                    total = total + (res[1] if res[0] else 0)
+        return total
