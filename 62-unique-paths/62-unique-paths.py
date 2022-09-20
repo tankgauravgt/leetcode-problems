5f5c616a24726a4fe45faf05b73bb4b4ccf632c0
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-09-20 13:36:58 +0530

    Time: 67 ms (11.71%), Space: 13.8 MB (98.19%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,17 +1,16 @@
-import math
-
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        dp = [[0] * n for mx in range(m)]
+        memo = {(0, 0): 1}
+        for rx in range(m):
+            for cx in range(n):
+                if (rx, cx) in memo:
+                    continue
+                if rx == 0:
+                    memo[(rx, cx)] = memo[(rx, cx - 1)]
+                elif cx == 0:
+                    memo[(rx, cx)] = memo[(rx - 1, cx)]
+                else:
+                    memo[(rx, cx)] = memo[(rx, cx - 1)] + memo[(rx - 1, cx)]
         
-        for mx in range(m):
-            dp[mx][0] = 1
-        for nx in range(n):
-            dp[0][nx] = 1
-        
-        for mx in range(1, m):
-            for nx in range(1, n):
-                dp[mx][nx] = dp[mx - 1][nx] + dp[mx][nx - 1]
-        
-        return dp[m - 1][n - 1]
+        return memo[(m - 1, n - 1)]
