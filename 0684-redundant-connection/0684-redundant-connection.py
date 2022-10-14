af9583816791ceae94117e0fedd28732c575ede4
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-10-14 13:38:46 +0530

    Time: 133 ms (21.75%), Space: 14.4 MB (91.87%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,26 @@
+class DSets:
+    
+    def __init__(self):
+        self.rec = {}
+    
+    def union(self, x, y):
+        self.rec[self.find(y)] = self.find(x)
+    
+    def find(self, x):
+        y = self.rec.get(x, x)
+        if y != x:
+            return self.find(y)
+        return y    
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        
+        rec = DSets()
+        
+        ans = []
+        for u, v in edges:
+            if rec.find(u) == rec.find(v):
+                ans = [u, v]
+            rec.union(u, v)
+            
+        return ans
