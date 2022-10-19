f1645541e35b046cd9e5b8ee809446b523427d6a
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-10-20 02:01:47 +0530

    Time: 6202 ms (5.01%), Space: 60.2 MB (5.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,9 +1,11 @@
-from collections import deque
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        def dp(rem, coins, memo):
+        coins.sort(reverse=True)
+        
+        memo = {}
+        def dp(rem, coins):
+            nonlocal memo
             if rem < 0:
                 return float('inf')
             elif rem == 0:
@@ -13,11 +15,11 @@ class Solution:
             else:
                 min_val = float('inf')
                 for coin in coins:
-                    min_val = min(min_val, 1 + dp(rem - coin, coins, memo))
+                    min_val = min(min_val, 1 + dp(rem - coin, coins))
                 memo[rem] = min_val
                 return min_val
         
-        res = dp(amount, coins, {})
+        res = dp(amount, coins)
         if res != float('inf'):
             return res
         return -1
