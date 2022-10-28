0e45ef4c5dc82dfaf274a25c4e9f4fb57ebfb384
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-10-28 19:10:21 +0530

    Time: 66 ms (13.60%), Space: 13.8 MB (98.98%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        
+        wordDict = list(wordDict)
+        
+        res = []
+        def rec(sx, buf, maxlen):
+            if sx == len(s):
+                res.append(" ".join(buf))
+            for ex in range(1 + sx, min(1 + sx + maxlen, 1 + len(s))):
+                if s[sx:ex] in wordDict:
+                    buf.append(s[sx:ex])
+                    rec(ex, buf, maxlen)
+                    buf.pop()
+            
+        maxlen = 0
+        for word in wordDict:
+            maxlen = max(maxlen, len(word))
+            
+        rec(0, [], maxlen)
+        return res
