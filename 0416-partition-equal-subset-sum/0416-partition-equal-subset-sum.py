ad4805f28dd76433f4d88fed955db07f37bc45c9
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-10-30 02:54:09 +0530

    Time: 3672 ms (33.82%), Space: 14.2 MB (77.23%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -2,24 +2,28 @@ class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         
         target = sum(nums)
-        
         if target % 2:
             return False
         
-        def dfs(nums, ix, csum, target, memo):
-            if csum == target:
+        buf = [[False for jx in range(1 + (target // 2))], None]
+        buf[0][0] = True
+        
+        for rx, n in enumerate(nums):
+            prev = None
+            curr = None
+            if rx % 2 == 0:
+                prev = buf[0]
+                buf[1] = curr = prev.copy()
+            else:
+                prev = buf[1]
+                buf[0] = curr = prev.copy()
+            
+            for sx in range(1 + target // 2):
+                if prev[sx] == True:
+                    if sx + n in range(0, 1 + (target // 2)):
+                        curr[sx + n] = True
+                        
+            if curr[target // 2]:
                 return True
-            elif csum > target:
-                return False
-            elif ix == len(nums):
-                return False
-            elif (ix, csum) in memo:
-                return memo[(ix, csum)]
-            flag = False
-            flag = flag or dfs(nums, 1 + ix, csum, target, memo)
-            if not flag:
-                flag = flag or dfs(nums, 1 + ix, csum + nums[ix], target, memo)
-            memo[(ix, csum)] = flag
-            return flag
-
-        return dfs(tuple(nums), 0, 0, target // 2, {})
+        
+        return False
