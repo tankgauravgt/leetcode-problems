ec8872780b08f91c0c5445aadb0fad3909eb4d6b
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-10-30 02:33:43 +0530

    Time: 7386 ms (10.45%), Space: 176.4 MB (7.61%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        
+        target = sum(nums)
+        
+        if target % 2:
+            return False
+        
+        def dfs(nums, ix, csum, target, memo):
+            if csum == target:
+                return True
+            elif csum > target:
+                return False
+            elif ix == len(nums):
+                return False
+            elif (ix, csum) in memo:
+                return memo[(ix, csum)]
+            flag = False
+            flag = flag or dfs(nums, 1 + ix, csum, target, memo)
+            flag = flag or dfs(nums, 1 + ix, csum + nums[ix], target, memo)
+            memo[(ix, csum)] = flag
+            return flag
+
+        return dfs(nums, 0, 0, target // 2, {})
