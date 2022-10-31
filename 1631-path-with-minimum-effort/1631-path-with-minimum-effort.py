ff636ae32c3d25acbeb3d9845e1c71f285310cf6
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-11-01 04:18:45 +0530

    Time: 3615 ms (20.77%), Space: 21 MB (12.03%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -0,0 +1,37 @@
+import heapq
+
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        
+        nR = len(heights)
+        nC = len(heights[0])
+        
+        def get_neighbors(rx, cx):
+            adjs = []
+            for dr, dc in [(0, -1), (0, +1), (-1, 0), (+1, 0)]:
+                if rx + dr in range(nR) and cx + dc in range(nC):
+                    diff = abs(heights[rx + dr][cx + dc] - heights[rx][cx])
+                    adjs.append(((rx + dr, cx + dc), diff))
+            return adjs
+        
+        G = {ix: [] for ix in range(nR * nC)}
+        for rx in range(nR):
+            for cx in range(nC):
+                for adj, weight in get_neighbors(rx, cx):
+                    G[nC * rx + cx].append((nC * adj[0] + adj[1], weight))
+        
+        distances = [float('inf')] * (nR * nC)
+        distances[0] = 0
+        hq = [(0, 0)]
+        
+        while hq:
+            dist, curr = heapq.heappop(hq)
+            if curr == nR * nC - 1:
+                return distances[curr]
+            for adj, weight in G[curr]:
+                if distances[adj] <= max(distances[curr], weight):
+                    continue
+                distances[adj] = max(distances[curr], weight)
+                heapq.heappush(hq, (distances[adj], adj))
+        
+        return float('inf')
