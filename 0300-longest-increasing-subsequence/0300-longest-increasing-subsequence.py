79cfc277a81dc9bf878f1cc9ebebb1bef86ec82b
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-11-02 05:41:50 +0530

    Time: 173 ms (78.34%), Space: 14.2 MB (44.85%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -2,14 +2,11 @@ import bisect
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        
         dp = [float('inf')] * (1 + len(nums))
         dp[0] = -float('inf')
-        
-        max_slot = 0
+        cmax = 0
         for ix, n in enumerate(nums):
-            slot = bisect.bisect_left(dp, n)
-            dp[slot] = n
-            max_slot = max(max_slot, slot)
-        
-        return max_slot
+            jx = bisect.bisect_left(dp, n)
+            dp[jx] = n
+            cmax = max(cmax, jx)
+        return cmax
