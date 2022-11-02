5ab51d8ad8f3be487dd44505a14e5151b319513e
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-11-02 17:09:47 +0530

    Time: 2161 ms (68.22%), Space: 62 MB (16.09%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
@@ -0,0 +1,25 @@
+import bisect
+
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        
+        dp = [[float('inf'), float('inf')]] * (1 + len(envelopes))
+        dp[0] = [-float('inf'), -float('inf')]
+                
+        def rbinsearch(arr, lx, rx, target):
+            if rx >= lx:
+                mid = lx + (rx - lx) // 2
+                if arr[mid][0] < target[0] and arr[mid][1] < target[1]:
+                    return rbinsearch(arr, mid + 1, rx, target)
+                else:
+                    return rbinsearch(arr, lx, mid - 1, target)
+            return lx
+        
+        cmax = 0
+        for ix, n in enumerate(envelopes):
+            jx = rbinsearch(dp, 0, len(dp) - 1, n)
+            dp[jx] = n
+            cmax = max(cmax, jx)
+        return cmax
