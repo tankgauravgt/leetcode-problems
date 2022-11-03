0a898c320ff5724735f7902fdbcaead7eb0da48f
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-11-04 04:44:52 +0530

    Time: 89 ms (61.99%), Space: 13.9 MB (62.29%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -3,20 +3,17 @@ import re
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
-        Np, Ns = len(p), len(s)
-        memo = [[-1] * (1 + Np) for sx in range(1 + Ns)]
-        
         def rex_match2(s, p, sx, px, memo):
             if px == len(p):
                 return sx == len(s)
-            if memo[sx][px] != -1:
-                return memo[sx][px]
+            if (sx, px) in memo:
+                return memo[(sx, px)]
             match = sx < len(s) and p[px] in {s[sx], '.'}
             if px < len(p) - 1 and p[1 + px] == '*':
-                memo[sx][px] = (rex_match2(s, p, sx, 2 + px, memo) or (match and rex_match2(s, p, 1 + sx, px, memo)))
-                return memo[sx][px]
+                memo[(sx, px)] = (rex_match2(s, p, sx, 2 + px, memo) or (match and rex_match2(s, p, 1 + sx, px, memo)))
+                return memo[(sx, px)]
             else:
-                memo[sx][px] = (match and rex_match2(s, p, 1 + sx, 1 + px, memo))
-                return memo[sx][px]
+                memo[(sx, px)] = (match and rex_match2(s, p, 1 + sx, 1 + px, memo))
+                return memo[(sx, px)]
         
-        return rex_match2(s, p, 0, 0, memo)
+        return rex_match2(s, p, 0, 0, {})
