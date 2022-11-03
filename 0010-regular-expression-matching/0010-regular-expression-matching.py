7157d64770ca1e16f725ba8d0ce6e927767df234
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-11-04 04:37:39 +0530

    Time: 38 ms (98.68%), Space: 14.1 MB (39.17%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -12,13 +12,17 @@ class Solution:
             else:
                 return match and rex_match(s[1::], p[1::])
         
-        def rex_match2(s, p, sx, px):
+        def rex_match2(s, p, sx, px, memo):
             if px == len(p):
                 return sx == len(s)
+            if (sx, px) in memo:
+                return memo[(sx, px)]
             match = sx < len(s) and p[px] in {s[sx], '.'}
             if px < len(p) - 1 and p[1 + px] == '*':
-                return rex_match2(s, p, sx, 2 + px) or (match and rex_match2(s, p, 1 + sx, px))
+                memo[(sx, px)] = (rex_match2(s, p, sx, 2 + px, memo) or (match and rex_match2(s, p, 1 + sx, px, memo)))
+                return memo[(sx, px)]
             else:
-                return match and rex_match2(s, p, 1 + sx, 1 + px)
+                memo[(sx, px)] = (match and rex_match2(s, p, 1 + sx, 1 + px, memo))
+                return memo[(sx, px)]
         
-        return rex_match2(s, p, 0, 0)
+        return rex_match2(s, p, 0, 0, {})
