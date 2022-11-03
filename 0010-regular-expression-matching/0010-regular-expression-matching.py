500792fd4b89ace0ca78f65f997193d3e073210f
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-11-04 04:37:55 +0530

    Time: 87 ms (63.96%), Space: 14 MB (62.29%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -3,15 +3,6 @@ import re
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
-        def rex_match(s, p):
-            if not p:
-                return not s
-            match = (s and p[0] in {s[0], '.'})
-            if len(p) > 1 and p[1] == '*':
-                return rex_match(s, p[2::]) or (match and rex_match(s[1::], p))
-            else:
-                return match and rex_match(s[1::], p[1::])
-        
         def rex_match2(s, p, sx, px, memo):
             if px == len(p):
                 return sx == len(s)
