f3d03d95cfa4228cc90fd2967a9dc4145770d6be
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-11-04 16:52:02 +0530

    Time: 181 ms (84.15%), Space: 15 MB (56.50%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -7,12 +7,13 @@ class Solution:
         rx = 1
         count = 0
         while lx < rx:
-            next_rx = -1
+            next_lx = rx
+            next_rx = rx
             for cx in range(lx, rx):
                 if cx == len(nums) - 1:
                     return count
                 next_rx = max(next_rx, 1 + cx + nums[cx])
-            lx = rx
+            lx = next_lx
             rx = next_rx
             count = count + 1
         
