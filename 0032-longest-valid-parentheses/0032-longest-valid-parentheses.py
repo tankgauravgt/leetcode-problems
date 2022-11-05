3a21fda0427dd82e3e80197882fdfea232b15c60
Gaurav Tank <code.tankgauravgt@gmail.com>
2022-11-05 07:21:05 +0530

    Time: 131 ms (11.09%), Space: 14.1 MB (77.36%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -4,12 +4,12 @@ class Solution:
         dp = [0] * len(s)
         
         cmax = 0
-        for ix, c in enumerate(s):
-            if c == ')':
-                if ix - 1 >= 0 and s[ix - 1] == '(':
-                    dp[ix] = 2 + (0 if ix < 2 else dp[ix - 2])
-                elif ix - dp[ix - 1] - 1 >= 0 and s[ix - 1 - dp[ix - 1]] == '(':
-                    dp[ix] = 2 + dp[ix - 1] + (0 if ix - dp[ix - 1] < 2 else dp[ix - dp[ix - 1] - 2])
-                cmax = max(cmax, dp[ix])
+        for ix in range(1, len(s)):
+            if s[ix - 1] == '(' and s[ix] == ')':
+                dp[ix] = 2 + (0 if ix - 2 < 0 else dp[ix - 2])
+            elif s[ix - 1] == ')' and s[ix] == ')':
+                if ix - 1 - dp[ix - 1] >= 0 and s[ix - 1 - dp[ix - 1]] == '(':
+                    dp[ix] = 2 + dp[ix - 1] + (0 if ix - 2 - dp[ix - 1] < 0 else dp[ix - 2 - dp[ix - 1]])
+            cmax = max(cmax, dp[ix])
         
         return cmax
